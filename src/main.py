d01819ec3efa300ed0a6284922dd3184d556868b
Beer-and-Skittles <jessicahu1012@gmail.com>
2023-03-19 17:04:58 +0800

    test

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ parser.add_argument('--cellname', type=str, default='AND2X1_RVT', help='cell-nam
 parser.add_argument('--jsondir', type=str, default='./in/', help='directory path to input json file(s)')
 parser.add_argument('--csvdir', type=str, default='./out/', help='directory path to output csv file(s)')
 parser.add_argument('--display', type=str, default='ON', help='\'ON\': display on, all else: display off')
-parser.add_argument('--iterations', type=int, default=4, help='iterations of data augmentation')
+parser.add_argument('--iterations', type=int, default=0, help='iterations of data augmentation')
 args = parser.parse_args()
 
 json2csv(
